a33d25dc9e108491302e2c1f0dfb489dd9635d9f
arcctgx <arcctgx@o2.pl>
2022-07-09 00:18:23 +0200

    mbtoc.py: add program for getting CD TOC from MB disc ID
    mbtoc.py gets disc info from MusicBrainz based on specified disc ID,
    and displays the list of track offsets and the total number of sectors.
    Maybe this information will be sufficient to calculatg FreeDB ID and
    AccurateRip disc IDs.
    
    Define application name and version string in a dedicated file.

---
+++ mbtoc.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+"""Get table of contents (TOC) from MusicBrainz disc ID."""
+
+import json
+import os
+import sys
+
+import musicbrainzngs
+import version
+
+
+def _get_toc(disc_id):
+    musicbrainzngs.set_useragent(version.APPNAME, version.VERSION)
+
+    try:
+        disc_data = musicbrainzngs.get_releases_by_discid(disc_id)
+    except musicbrainzngs.ResponseError:
+        return None
+
+    toc = {}
+    toc['offset-list'] = disc_data['disc']['offset-list']
+    toc['sectors'] = disc_data['disc']['sectors']
+
+    return toc
+
+
+def main():
+    if len(sys.argv) != 2:
+        print(f'usage: {os.path.basename(sys.argv[0])} <discID>')
+        sys.exit(1)
+
+    disc_id = sys.argv[1]
+    toc = _get_toc(disc_id)
+
+    if toc:
+        print(json.dumps(toc, indent=2))
+    else:
+        print(f'Failed to get CD data matching discID "{disc_id}"!')
+
+
+if __name__ == '__main__':
+    main()
---
+++ version.py
@@ -0,0 +1,4 @@
+"""Application name and version string."""
+
+APPNAME = "ARVer"
+VERSION = "v0.1.0-dev"
