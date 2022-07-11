de0b1aaac778ef72f900dae74f6b5fb6c5e9b191
arcctgx <arcctgx@o2.pl>
2022-07-12 00:03:30 +0200

    cdtoc.py: add program for reading TOC from a CD in drive
    Update requirements.txt.

---
+++ cdtoc.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+"""Get Table of Contents (TOC) from a physical CD in drive."""
+
+import json
+import discid
+
+
+def _get_toc():
+    try:
+        disc = discid.read()
+    except discid.DiscError:
+        return None
+
+    toc = {}
+    toc['discid'] = disc.id
+    toc['freedb-id'] = disc.freedb_id
+    toc['offset-list'] = [track.offset for track in disc.tracks]
+    toc['sectors'] = disc.sectors
+
+    return toc
+
+
+def main():
+    toc = _get_toc()
+
+    if toc:
+        print(json.dumps(toc, indent=2))
+    else:
+        print('Failed to get the table of contents. Is there a CD in the drive?')
+
+
+if __name__ == '__main__':
+    main()
