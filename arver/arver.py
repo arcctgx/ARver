82e3e0fa86faf83d75e8fc3343b0ceadaf443b6a
arcctgx <arcctgx@o2.pl>
2022-08-25 00:58:06 +0200

    use argparse in arver.py
    Add options for selecting TOC source. This is not fully functional yet.

--- arver/arver.py
+++ arver/arver.py
@@ -2,13 +2,32 @@
 Verify the set of WAV files ripped from CD.
 """
 
+import argparse
 import sys
 
 from arver.rip.rip import Rip
 
 
+def _parse_args():
+    parser = argparse.ArgumentParser(
+        description="""Calculate AccurateRip checksums of WAV files and compare
+        calculated checksums with ones downloaded from AccurateRip database.""")
+
+    parser.add_argument('wav_files', metavar='wav_file', nargs='+',
+        help='WAV file to verify')
+
+    toc_sources = parser.add_mutually_exclusive_group()
+    toc_sources.add_argument('-d', '--drive',
+        help='get TOC from CD in drive')
+    toc_sources.add_argument('-i', '--disc-id',
+        help='get TOC from MusicBrainz by Disc ID')
+
+    return parser.parse_args()
+
+
 def main():
-    print(Rip(sys.argv[1:]))
+    args = _parse_args()
+    print(Rip(args.wav_files))
 
 
 if __name__ == '__main__':
