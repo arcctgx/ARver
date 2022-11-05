807c3e23f5ac302910cb505e663fc31c0eb3b239
arcctgx <arcctgx@o2.pl>
2022-11-05 01:13:26 +0100

    arver-ripinfo: use argparse
    Use argparse instead of handling sys.argv. Improve usage message.
    
    Add -v/--version option for displaying version string.

--- arver/ripinfo.py
+++ arver/ripinfo.py
@@ -3,18 +3,32 @@ Display properties of a set of WAV files, including copy CRCs and
 two types of AccurateRip checksums.
 """
 
-import os
+import argparse
 import sys
 
+from arver import VERSION
 from arver.rip.rip import Rip
 
 
-def main():
-    if len(sys.argv) < 2:
-        print(f'usage: {os.path.basename(sys.argv[0])} <file> [file ...]')
-        sys.exit(1)
+def _parse_args():
+    parser = argparse.ArgumentParser(
+        description="""Display properties and checksums of specified audio
+        files. Calculation of AccurateRip checksums requires correct track
+        sequence, so files must be specified in the correct order. Pre-gap
+        track (HTOA) must not be included. Non-audio files and unsupported
+        audio formats are quietly ignored.""")
 
-    rip = Rip(sys.argv[1:])
+    parser.add_argument('rip_files', nargs='+', metavar='file',
+        help='audio file for calculating checksums')
+    parser.add_argument('-v', '--version', action='version', version=VERSION)
+
+    return parser.parse_args()
+
+
+def main():
+    args = _parse_args()
+
+    rip = Rip(args.rip_files)
     if rip.num_tracks == 0:
         print('No WAV files were loaded. Did you specify correct files?')
         sys.exit(1)
