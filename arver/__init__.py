87a84a360e28a64aaf6ef79da3e4401ed298d7a3
arcctgx <arcctgx@o2.pl>
2022-12-29 23:38:16 +0100

    handle AccurateRip checksum calculation failure
    When accuraterip.compute() fails to calculate AccurateRip checksums
    it returns None instead of an int. This can happen when the audio file
    cannot be loaded, or is not CDDA (not stereo, with wrong bit depth or
    sampling rate). This case was not handled, and resulted in a crash when
    AccurateRip checksum was printed.
    
    Change the Python wrapper for compute() so that it returns a pair of
    zeros in case of error. The condition checks if any of the returned
    checksums is None, and this is sufficient. The calculation will either
    succeed for both checksum types, or it will fail for both - there is no
    middle ground.
    
    Using zero as a placeholder for an incorrect checksum seems reasonable,
    because AccurateRip database uses the same value for missing checksums.
    I'm not sure if it's possible to calculate a zero value checksum for a
    track with non-zero length anyway.

--- arver/__init__.py
+++ arver/__init__.py
@@ -1,5 +1,5 @@
 """Main arver package."""
 
 APPNAME = 'ARver'
-VERSION = 'v0.3.0'
+VERSION = 'v0.3.1-dev'
 URL = 'https://github.com/arcctgx/ARver'
--- arver/checksum/checksum.py
+++ arver/checksum/checksum.py
@@ -3,10 +3,12 @@
 import binascii
 import wave
 
+from typing import Tuple
+
 from arver.checksum import accuraterip  # type: ignore
 
 
-def accuraterip_checksums(wav_file, track_no, total_tracks):
+def accuraterip_checksums(wav_file, track_no, total_tracks) -> Tuple[int, int]:
     """
     Calculate AccurateRip v1 and v2 checksums of specified file.
     WAV and FLAC formats are supported.
@@ -15,10 +17,16 @@ def accuraterip_checksums(wav_file, track_no, total_tracks):
     if the track is the first or the last one on CD. These two
     tracks are treated specially by AccurateRip checksum algorithm.
 
-    Return a pair of checksums (v1, v2) as unsigned integers.
+    Return a pair of checksums (v1, v2) as unsigned integers, or a
+    pair of zeros if an error occurred during checksum calculation.
     """
     # pylint: disable=c-extension-no-member
-    return accuraterip.compute(wav_file, track_no, total_tracks)
+    ar1, ar2 = accuraterip.compute(wav_file, track_no, total_tracks)
+
+    if ar1 is None or ar2 is None:
+        return 0x0, 0x0
+
+    return ar1, ar2
 
 
 def copy_crc(wav_file):
