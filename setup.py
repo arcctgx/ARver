c3f99c8e1e70d942fabe8676a03b8402ad478e52
arcctgx <arcctgx@o2.pl>
2023-09-11 00:34:39 +0200

    update package metadata
    Add long description, Python Trove classifiers, minimum Python version
    and dependencies.
    
    Development status is still beta because no Python wheels are provided,
    so installation requires gcc and libsndfile headers.

--- setup.py
+++ setup.py
@@ -1,13 +1,30 @@
 from setuptools import setup, find_packages, Extension
-
 from arver import APPNAME, VERSION, URL
+
+with open('README.md', encoding='utf-8') as f:
+    readme = f.read()
 
 setup(name=APPNAME,
       version=VERSION,
-      description='Application for verifiyng ripped WAV files using AccurateRip database.',
+      description='Application for verifying ripped audio files using AccurateRip database.',
+      long_description=readme,
+      long_description_content_type="text/markdown",
       author='arcctgx',
+      author_email='arcctgx@o2.pl',
       url=URL,
       license='GPLv3',
+      classifiers=[
+          'Development Status :: 4 - Beta', 'Environment :: Console',
+          'Intended Audience :: End Users/Desktop',
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          'Natural Language :: English', 'Operating System :: POSIX :: Linux',
+          'Programming Language :: C', 'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8', 'Programming Language :: Python :: 3.9',
+          'Programming Language :: Python :: 3.10', 'Programming Language :: Python :: 3.11',
+          'Topic :: Multimedia :: Sound/Audio :: Analysis',
+          'Topic :: Multimedia :: Sound/Audio :: CD Audio :: CD Ripping'
+      ],
+      python_requires='>=3.7',
       packages=find_packages(),
       entry_points={
           'console_scripts': [
@@ -19,4 +36,5 @@ setup(name=APPNAME,
           Extension('arver.checksum.accuraterip',
                     sources=['arver/checksum/accuraterip.c'],
                     libraries=['sndfile'])
-      ])
+      ],
+      install_requires=['discid', 'musicbrainzngs', 'pycdio', 'requests'])
