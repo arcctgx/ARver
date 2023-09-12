c43df3ceffbef207c29d489782c0f124d18e62b0
arcctgx <arcctgx@o2.pl>
2023-09-12 02:25:52 +0200

    add script for building wheels
    Adapted from pypa/python-manylinux-demo. Platform is hardcoded instead
    of using an environment variable, because I don't think I will provide
    wheels other than manylinux2014_x86_64 type.
    
    Testing wheels in the script proved much more difficult than I expected:
    it's not possible to install ARver in the container because pycdio can't
    be installed from wheels (none are provided) nor source (libcdio version
    available in the container is too old). I'm not sure I want to spend
    time trying to figure out how to do this.

---
+++ utils/build-wheels.sh
@@ -0,0 +1,39 @@
+#!/bin/bash -eux
+
+# Script for building manylinux wheels. Adapted from pypa/python-manylinux-demo.
+# This script is intended to be executed inside manylinux container, e.g.:
+#
+# docker run --rm -v "$(pwd)":/package \
+#   quay.io/pypa/manylinux2014_x86_64 \
+#   /package/utils/build-wheels.sh
+#
+
+platform="manylinux2014_x86_64"
+package_dir="/package"
+wheel_dir="${package_dir}/wheelbarrow"
+
+# start with clean state:
+rm -rf "${package_dir}/build" "${wheel_dir}"
+
+# ARver requires Python >= 3.7, so remove Python-3.6 symlink:
+rm -rf /opt/python/cp36-cp36m
+
+# install system packages required to build C extensions:
+yum install -y libsndfile-devel
+
+# build CPython wheels:
+for pybin in /opt/python/cp*/bin
+do
+    "${pybin}/pip" wheel "${package_dir}" --no-deps --wheel-dir "${wheel_dir}"
+done
+
+# vendor required shared libraries in wheels:
+for wheel in "${wheel_dir}"/*.whl
+do
+    if ! auditwheel show "${wheel}"; then
+        echo "Skipping non-platform wheel ${wheel}"
+        continue
+    fi
+
+    auditwheel repair "${wheel}" --plat "${platform}" --wheel-dir "${wheel_dir}"
+done
