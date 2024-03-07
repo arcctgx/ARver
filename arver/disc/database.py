b92ecfb286f0035d930a5b2997b8f4c5af32a352
arcctgx <arcctgx@o2.pl>
2024-03-08 00:44:59 +0100

    handle ConnectTimeout when downloading AccurateRip data
    The database can be down sometimes. Until now it resulted in huge stack
    trace being printed to the console. Handle this exception and print an
    appropriate error message followed by the rip info table.

--- arver/disc/database.py
+++ arver/disc/database.py
@@ -290,6 +290,8 @@ class AccurateRipFetcher:
             response.raise_for_status()
             self._raw_bytes = response.content
             return self._parse_raw_bytes()
+        except requests.ConnectTimeout:
+            print('Failed to connect to AccurateRip database. Try again later.')
         except requests.HTTPError as error:
             if error.response.status_code == 404:
                 print('Could not find disc in AccurateRip database.')
