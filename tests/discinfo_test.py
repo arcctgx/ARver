0cdc5e1937c3816ae102e73e15881fe9779c05f4
arcctgx <arcctgx@o2.pl>
2024-03-18 01:44:40 +0100

    avoid sending requests to MusicBrainz in tests
    get_releases_by_discid() returns release data in JSON format, so this
    data can be easily stored in a file. Store information about several
    discs as test data, and mock get_releases_by_discid() calls to return
    test release data loaded from JSON files.
    
    Change one of the discs used in test: its disc ID began with a period,
    so its JSON file would be hidden. The new disc has the maximum number
    of tracks possible in an audio CD (in addition to a pregap).
    
    Remove assertion for checking if the returned object is not None. This
    won't happen if the test is not sending requests to MusicBrainz anymore.
    
    Avoiding network overhead makes the test execute almost instantly
    instead of taking a few seconds. I'm still not sure about the idea
    of this test - it seems to me that it doesn't actually test very much.

--- tests/discinfo_test.py
+++ tests/discinfo_test.py
@@ -1,31 +1,40 @@
 """Tests of DiscInfo class."""
 
+import json
+import os
 import unittest
+from unittest.mock import patch
 
 from arver.disc.info import DiscInfo
+
+CWD = os.path.abspath(os.path.dirname(__file__))
 
 
 class TestDiscInfo(unittest.TestCase):
     """Tests of DiscInfo class."""
 
-    def test_discid_recalculation(self):
+    @patch('musicbrainzngs.get_releases_by_discid')
+    def test_discid_recalculation(self, mock_get):
         """
         When DiscInfo instance is created from a disc ID its musicbrainz_id()
         method should return the same disc ID. Test DiscID calculation with
         three different types of discs.
-        Note: this test will send requests to MusicBrainz.
         """
         discids = [
             'X_fUy2PVsk5KK7JciFqBc0CetiI-',  # Audio CD
-            '.wsrLgOecMphb09w1pr.ZwcIrj8-',  # Audio CD with pregap
+            'RQ9yuzEHF_fzexjCzj46KcdIaHA-',  # Audio CD with pregap and 99 tracks
             'j2wJHXA1xXZj7Wkh_tKm4vLjqBk-',  # Mixed Mode CD
             'icmg5lhDisLpCIHbXmBhXUDlN2I-',  # Enhanced CDs
             'ZvhSySPNhWlmkC1x3pEYDnMyoho-'
         ]
 
         for id_ in discids:
+            disc_data = f'{CWD}/data/discs/{id_}.json'
+            with open(disc_data, encoding='utf-8') as disc:
+                mock_disc = json.load(disc)
+                mock_get.return_value = mock_disc
+
             disc_info = DiscInfo.from_disc_id(id_)
-            self.assertIsNotNone(disc_info)
             self.assertEqual(id_, disc_info.musicbrainz_id())
 
 
