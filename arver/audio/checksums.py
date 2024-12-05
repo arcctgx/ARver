e9dfdaf0fc6859e7a6ffa72c51e1481c9464562e
arcctgx <arcctgx@o2.pl>
2024-12-05 01:57:19 +0100

    return computed checksums as a dictionary
    Adapt the code and tests.
    
    Not sure yet if that's actually an improvement.

--- arver/audio/checksums.py
+++ arver/audio/checksums.py
@@ -1,17 +1,25 @@
 """Functions for calculating checksums of audio files."""
 
-from typing import Tuple
+from typing import Dict
 
 from arver.audio import _audio  # type: ignore
 
 # pylint: disable=c-extension-no-member
 
 
-def get_checksums(path: str, track_no: int, total_tracks: int) -> Tuple[int, int, int, int]:
+def get_checksums(path: str, track_no: int, total_tracks: int) -> Dict[str, int]:
     """
     Calculate AccurateRip and CRC32 checksums of specified file.
-    Return a tuple of checksums (ARv1, ARv2, CRC32, skip silence
-    CRC32) as unsigned integers.
+    Return a dictionary of checksums (ARv1, ARv2, CRC32, skip
+    silence CRC32) stored as unsigned integers. The following
+    example demonstrates the dictionary structure and key names:
+
+    {
+        "ar1": 1015927250,
+        "ar2": 1455137394,
+        "crc": 2364014889,
+        "crcss": 760217820
+    }
 
     This function supports WAV and FLAC files compliant with CDDA
     standard (16-bit stereo LPCM, 44100 Hz). Underlying C extension
--- tests/checksums_test.py
+++ tests/checksums_test.py
@@ -17,57 +17,57 @@ SILENCE_WAV_PATH = CWD + '/data/samples/silence.wav'
 class TestAccurateRip(unittest.TestCase):
     """Test calculation of AccurateRip checksums."""
 
-    only_track = (0xf43f9174, 0x0ae7c6f9)
-    first_track = (0x9d6c90ec, 0xb775893e)
-    middle_track = (0x3c8dd1d2, 0x56bba272)
-    last_track = (0x9360d25a, 0xaa2de02d)
-    silence = (0x0, 0x0)
+    only_track = {'ar1': 0xf43f9174, 'ar2': 0x0ae7c6f9}
+    first_track = {'ar1': 0x9d6c90ec, 'ar2': 0xb775893e}
+    middle_track = {'ar1': 0x3c8dd1d2, 'ar2': 0x56bba272}
+    last_track = {'ar1': 0x9360d25a, 'ar2': 0xaa2de02d}
+    silence = {'ar1': 0x0, 'ar2': 0x0}
 
     def test_wav_only_track(self):
         result = get_checksums(SAMPLE_WAV_PATH, 1, 1)
-        accuraterip = result[:2]
-        self.assertTupleEqual(accuraterip, self.only_track)
+        accuraterip = {key: result[key] for key in ('ar1', 'ar2')}
+        self.assertEqual(accuraterip, self.only_track)
 
     def test_wav_first_track(self):
         result = get_checksums(SAMPLE_WAV_PATH, 1, 99)
-        accuraterip = result[:2]
-        self.assertTupleEqual(accuraterip, self.first_track)
+        accuraterip = {key: result[key] for key in ('ar1', 'ar2')}
+        self.assertEqual(accuraterip, self.first_track)
 
     def test_wav_middle_track(self):
         result = get_checksums(SAMPLE_WAV_PATH, 10, 99)
-        accuraterip = result[:2]
-        self.assertTupleEqual(accuraterip, self.middle_track)
+        accuraterip = {key: result[key] for key in ('ar1', 'ar2')}
+        self.assertEqual(accuraterip, self.middle_track)
 
     def test_wav_last_track(self):
         result = get_checksums(SAMPLE_WAV_PATH, 99, 99)
-        accuraterip = result[:2]
-        self.assertTupleEqual(accuraterip, self.last_track)
+        accuraterip = {key: result[key] for key in ('ar1', 'ar2')}
+        self.assertEqual(accuraterip, self.last_track)
 
     def test_flac_only_track(self):
         result = get_checksums(SAMPLE_FLAC_PATH, 1, 1)
-        accuraterip = result[:2]
-        self.assertTupleEqual(accuraterip, self.only_track)
+        accuraterip = {key: result[key] for key in ('ar1', 'ar2')}
+        self.assertEqual(accuraterip, self.only_track)
 
     def test_flac_first_track(self):
         result = get_checksums(SAMPLE_FLAC_PATH, 1, 99)
-        accuraterip = result[:2]
-        self.assertTupleEqual(accuraterip, self.first_track)
+        accuraterip = {key: result[key] for key in ('ar1', 'ar2')}
+        self.assertEqual(accuraterip, self.first_track)
 
     def test_flac_middle_track(self):
         result = get_checksums(SAMPLE_FLAC_PATH, 10, 99)
-        accuraterip = result[:2]
-        self.assertTupleEqual(accuraterip, self.middle_track)
+        accuraterip = {key: result[key] for key in ('ar1', 'ar2')}
+        self.assertEqual(accuraterip, self.middle_track)
 
     def test_flac_last_track(self):
         result = get_checksums(SAMPLE_FLAC_PATH, 99, 99)
-        accuraterip = result[:2]
-        self.assertTupleEqual(accuraterip, self.last_track)
+        accuraterip = {key: result[key] for key in ('ar1', 'ar2')}
+        self.assertEqual(accuraterip, self.last_track)
 
     def test_silence(self):
         for track, total in [(1, 1), (1, 99), (10, 99), (99, 99)]:
             result = get_checksums(SILENCE_WAV_PATH, track, total)
-            accuraterip = result[:2]
-            self.assertTupleEqual(accuraterip, self.silence)
+            accuraterip = {key: result[key] for key in ('ar1', 'ar2')}
+            self.assertEqual(accuraterip, self.silence)
 
 
 class TestCrc32(unittest.TestCase):
@@ -80,25 +80,25 @@ class TestCrc32(unittest.TestCase):
 
     track = 5
     total = 10
-    sample_crc32 = (0x8ce80129, 0x2d5000dc)
-    silence_crc32 = (0x769282bb, 0x0)
+    sample = {'crc': 0x8ce80129, 'crcss': 0x2d5000dc}
+    silence = {'crc': 0x769282bb, 'crcss': 0x0}
 
     def test_wav(self):
         result = get_checksums(SAMPLE_WAV_PATH, self.track, self.total)
-        crc = result[2:]
-        self.assertTupleEqual(crc, self.sample_crc32)
+        crc = {key: result[key] for key in ('crc', 'crcss')}
+        self.assertEqual(crc, self.sample)
 
     def test_wav_big_endian(self):
         result = get_checksums(SAMPLE_BE_WAV_PATH, self.track, self.total)
-        crc = result[2:]
-        self.assertTupleEqual(crc, self.sample_crc32)
+        crc = {key: result[key] for key in ('crc', 'crcss')}
+        self.assertEqual(crc, self.sample)
 
     def test_flac(self):
         result = get_checksums(SAMPLE_FLAC_PATH, self.track, self.total)
-        crc = result[2:]
-        self.assertTupleEqual(crc, self.sample_crc32)
+        crc = {key: result[key] for key in ('crc', 'crcss')}
+        self.assertEqual(crc, self.sample)
 
     def test_silence(self):
         result = get_checksums(SILENCE_WAV_PATH, self.track, self.total)
-        crc = result[2:]
-        self.assertTupleEqual(crc, self.silence_crc32)
+        crc = {key: result[key] for key in ('crc', 'crcss')}
+        self.assertEqual(crc, self.silence)
