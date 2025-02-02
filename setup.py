9d3554b0fe173d0192bd115d7a4a76cadbf28c75
arcctgx <arcctgx@o2.pl>
2025-02-02 23:02:02 +0100

    enable optimizations when building the C extension
    The -O3 optimization option has been enabled so far, but it was only
    by chance. The compiler and linker options actually used for building
    depend on multiple factors, and they can differ between Linux distros
    even for the same Python interpreter version.
    
    Explicitly adding -O3 option to extra_compile_args ensures it will be
    enabled in all environments.

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(name=APPNAME,
           Extension('arver.audio._audio',
                     sources=['arver/audio/_audio.c'],
                     libraries=['sndfile', 'z'],
-                    extra_compile_args=['-std=c99', '-D_DEFAULT_SOURCE'],
+                    extra_compile_args=['-std=c99', '-O3', '-D_DEFAULT_SOURCE'],
                     define_macros=[('Py_LIMITED_API', '0x03070000')],
                     py_limited_api=True)
       ],
