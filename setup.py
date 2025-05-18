fd49f8ff8b6a1c5198964ce3b97c90f1ad7b2350
arcctgx <arcctgx@o2.pl>
2025-05-18 21:11:41 +0200

    target Python 3.7 limited API/stable ABI
    It turns out that the C extension code generated by SWIG doesn't use
    anything outside the Python 3.7 limited API/stable ABI - at least for
    now.
    
    Verified by building bdist and verifying it using abi3audit.

--- setup.py
+++ setup.py
@@ -45,7 +45,9 @@ setup(name=APPNAME,
           Extension('arver.minicdio._minicdio',
                     sources=['arver/minicdio/_minicdio.c'],
                     libraries=['cdio'],
-                    extra_compile_args=['-std=c99', '-O3'])
+                    extra_compile_args=['-std=c99', '-O3'],
+                    define_macros=[('Py_LIMITED_API', '0x03070000')],
+                    py_limited_api=True)
       ],
       options={'bdist_wheel': {
           'py_limited_api': 'cp37'
