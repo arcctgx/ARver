26ee25951600e280b3ed0b735dde44d35a7e1cc3
arcctgx <arcctgx@o2.pl>
2025-06-02 20:40:51 +0200

    migrate project settings to pyproject.toml
    Move essential settings from setup.py to pyproject.toml, leaving only
    the definition of the C extension. Add some trailing commas in the
    setup() call to tell YAPF not to collapse the function arguments too
    much. Add module docstring to setup.py.
    
    Previously the source distribution did not include any test code, but
    now the top-level tests directory is added automatically (just the top
    level: test code without test data). Add MANIFEST.in with an explicit
    removal of tests directory to keep the source distribution the same as
    it was before.
    
    I'm not using a PEP 639 compliant license expression in pyproject.toml
    yet. This is because of supported Python version constraints: the most
    recent setuptools version that still supports Python 3.7 is 68.0.0,
    while the PEP 639 license expressions are only supported from 77.0.0.
    
    I guess I'll have to live with the deprecation warnings until I drop
    support for the older Python versions.

--- setup.py
+++ setup.py
@@ -1,49 +1,19 @@
-from setuptools import Extension, find_packages, setup
+"""ARver C extension definition."""
 
-from arver import APPNAME, URL, VERSION
+from setuptools import Extension, setup
 
-with open('README.md', encoding='utf-8') as f:
-    readme = f.read()
-
-setup(name=APPNAME,
-      version=VERSION,
-      description='Application for verifying ripped audio files using AccurateRip database.',
-      long_description=readme,
-      long_description_content_type="text/markdown",
-      author='arcctgx',
-      author_email='arcctgx@o2.pl',
-      url=URL,
-      license='GPLv3',
-      classifiers=[
-          'Development Status :: 5 - Production/Stable', 'Environment :: Console',
-          'Intended Audience :: End Users/Desktop',
-          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-          'Natural Language :: English', 'Operating System :: POSIX :: Linux',
-          'Programming Language :: C', 'Programming Language :: Python :: 3 :: Only',
-          'Programming Language :: Python :: 3.7', 'Programming Language :: Python :: 3.8',
-          'Programming Language :: Python :: 3.9', 'Programming Language :: Python :: 3.10',
-          'Programming Language :: Python :: 3.11', 'Programming Language :: Python :: 3.12',
-          'Topic :: Multimedia :: Sound/Audio :: Analysis',
-          'Topic :: Multimedia :: Sound/Audio :: CD Audio :: CD Ripping'
-      ],
-      python_requires='>=3.7',
-      packages=find_packages(),
-      entry_points={
-          'console_scripts': [
-              'arver = arver.arver_main:main', 'arver-discinfo = arver.arver_discinfo:main',
-              'arver-ripinfo = arver.arver_ripinfo:main',
-              'arver-bin-parser = arver.arver_bin_parser:main'
-          ]
-      },
-      ext_modules=[
-          Extension('arver.audio._audio',
-                    sources=['arver/audio/_audio.c'],
-                    libraries=['sndfile', 'z'],
-                    extra_compile_args=['-std=c99', '-O3', '-D_DEFAULT_SOURCE'],
-                    define_macros=[('Py_LIMITED_API', '0x03070000')],
-                    py_limited_api=True)
-      ],
-      options={'bdist_wheel': {
-          'py_limited_api': 'cp37'
-      }},
-      install_requires=['discid', 'musicbrainzngs', 'pycdio', 'requests'])
+setup(
+    ext_modules=[
+        Extension('arver.audio._audio',
+                  sources=['arver/audio/_audio.c'],
+                  libraries=['sndfile', 'z'],
+                  extra_compile_args=['-std=c99', '-O3', '-D_DEFAULT_SOURCE'],
+                  define_macros=[('Py_LIMITED_API', '0x03070000')],
+                  py_limited_api=True),
+    ],
+    options={
+        'bdist_wheel': {
+            'py_limited_api': 'cp37'
+        },
+    },
+)
