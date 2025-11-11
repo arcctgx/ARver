fd862c06766fdc4fb61a570c04ecd2edca690e4d
arcctgx <arcctgx@o2.pl>
2025-11-11 21:42:01 +0100

    fix release scripting
    PyPI now requires correctly normalized distribution names, which the
    stack based on Python 3.7 simply doesn't provide, so my latest uploads
    were getting rejected.
    
    Move building both source and built distributions to the PyPA container.
    Adapt the build script to use a more recent Python version and remove
    unneeded targets from the Makefile. Create new arcctgx/arver-builder
    container version.
    
    The build script uses Python 3.8 because it's new enough to produce
    correctly normalized distribution names, but still old enough not to
    cause problems with other elements of my outdated Python 3.7 stack.
    
    At this point trying to still support Python versions older than 3.10
    is just fighting obsolete tooling. It's just not feasible anymore.

--- Makefile
+++ Makefile
@@ -1,23 +1,18 @@
-.PHONY: release image wheels test sdist clean gitclean
+.PHONY: release image test clean gitclean
 
 CWD = $(shell pwd -P)
 USER = $(shell id -u)
 GROUP = $(shell id -g)
 VERSION = $(shell cat utils/IMAGE_VERSION)
 
-release: sdist wheels
-
 image:
 	docker build --tag arcctgx/arver-builder:$(VERSION) .
 
-wheels:
+release:
 	docker run --rm -u "$(USER):$(GROUP)" -v "$(CWD):/package" arcctgx/arver-builder:$(VERSION)
 
 test:
 	python3 -m unittest discover -v -s tests/ -p "*_test.py"
-
-sdist:
-	python3 -m build --sdist
 
 clean:
 	$(RM) -vrf build dist wheelhouse
--- utils/build-wheels.sh
+++ utils/build-wheels.sh
@@ -10,7 +10,7 @@
 
 platform="manylinux2014_x86_64"
 package_dir="/package"
-wheel_dir="${package_dir}/dist"
+dist_dir="${package_dir}/dist"
 
 if [[ ! -d "${package_dir}" ]]; then
     echo "The directory ${package_dir} is not available inside container."
@@ -18,20 +18,20 @@ if [[ ! -d "${package_dir}" ]]; then
 fi
 
 # start with clean state:
-rm -rf "${package_dir}/build" "${wheel_dir}/*.whl"
+rm -rf "${package_dir}/build" "${dist_dir}/*"
 
 # Py_LIMITED_API 0x03070000 is defined, so it's sufficient to build
-# just one wheel with Python 3.7:
-python3.7 -m pip wheel "${package_dir}" --wheel-dir "${wheel_dir}"
+# just one wheel with any Python version:
+python3.8 -m build "${package_dir}" --outdir "${dist_dir}"
 
 # vendor required shared libraries in wheels:
-for wheel in "${wheel_dir}"/*.whl
+for wheel in "${dist_dir}"/*.whl
 do
     if ! auditwheel show "${wheel}"; then
         echo "Skipping non-platform wheel ${wheel}"
         continue
     fi
 
-    auditwheel repair "${wheel}" --plat "${platform}" --strip --wheel-dir "${wheel_dir}"
+    auditwheel repair "${wheel}" --plat "${platform}" --strip --wheel-dir "${dist_dir}"
     rm -v "${wheel}"
 done
