2e3da324b4129070e5c3094561d1265bdaff6d49
arcctgx <arcctgx@o2.pl>
2026-04-26 03:28:49 +0200

    allow build-wheels.sh to run in any PyPA container
    Up to now build-wheels.sh was executed in a custom container based on
    PyPA manylinux2014_x86_64 image, with libsndfile and its dependencies
    pre-built. This worked fine, but it restricted the builds to a single
    platform.
    
    Using the stock PyPA container and compiling all dependencies at runtime
    allows executing the build script in any PyPA container where libsndfile
    and its dependencies can be built from source. It was tested with 32-
    and 64-bit manylinux2014 and musllinux_1_2, but in principle it could
    be used to build wheels for other platforms as well (e.g. aarch64).
    
    Running ldconfig is not necessary in musllinux containers, so the build
    script skips ldconfig when musl is detected. Otherwise ldconfig would
    exit with non-zero status, breaking the build.
    
    Remove the Dockerfile and supporting files. Remove unnecessary targets
    from the Makefile and update the release target.

--- Makefile
+++ Makefile
@@ -1,15 +1,9 @@
-.PHONY: release image test clean gitclean
+.PHONY: release test clean gitclean
 
 CWD = $(shell pwd -P)
-USER = $(shell id -u)
-GROUP = $(shell id -g)
-VERSION = $(shell cat utils/IMAGE_VERSION)
-
-image:
-	docker build --tag arcctgx/arver-builder:$(VERSION) .
 
 release:
-	docker run --rm -u "$(USER):$(GROUP)" -v "$(CWD):/package" arcctgx/arver-builder:$(VERSION)
+	docker run --rm -e PLAT=manylinux2014_x86_64 -v $(CWD):/pkg quay.io/pypa/manylinux2014_x86_64 /pkg/utils/build-wheels.sh
 
 test:
 	python3 -m unittest discover -v -s tests/ -p "*_test.py"
--- utils/build-wheels.sh
+++ utils/build-wheels.sh
@@ -1,16 +1,19 @@
 #!/bin/bash -eux
 
 # Script for building manylinux wheels. Adapted from pypa/python-manylinux-demo.
-# This script is the entry point of arcctgx/arver-builder container. It expects
-# package sources to be available in /package directory inside the container.
-# This can be achieved with Docker bind mount, e.g.:
+# This script is executed in one of the PyPA manylinux containers. It assumes
+# package sources are available in /pkg directory inside the container.
 #
-# docker run --rm -u 1000:100 -v "$(pwd):/package" arcctgx/arver-builder
+# Usage examples:
 #
+# docker run --rm -e PLAT=manylinux2014_x86_64 -v $(pwd -P):/pkg quay.io/pypa/manylinux2014_x86_64 /pkg/utils/build-wheels.sh
+# docker run --rm -e PLAT=manylinux2014_i686 -v $(pwd -P):/pkg quay.io/pypa/manylinux2014_i686 linux32 /pkg/utils/build-wheels.sh
+# docker run --rm -e PLAT=musllinux_1_2_x86_64 -v $(pwd -P):/pkg quay.io/pypa/musllinux_1_2_x86_64 /pkg/utils/build-wheels.sh
+# docker run --rm -e PLAT=musllinux_1_2_i686 -v $(pwd -P):/pkg quay.io/pypa/musllinux_1_2_i686 /pkg/utils/build-wheels-new.sh
 
-platform="manylinux2014_x86_64"
-package_dir="/package"
+package_dir="/pkg"
 dist_dir="${package_dir}/dist"
+wheel_dir="${package_dir}/wheelhouse"
 
 if [[ ! -d "${package_dir}" ]]; then
     echo "The directory ${package_dir} is not available inside container."
@@ -18,11 +21,63 @@ if [[ ! -d "${package_dir}" ]]; then
 fi
 
 # start with clean state:
-rm -rf "${package_dir}/build" "${dist_dir}/*"
+rm -rfv "${dist_dir}"
+
+# build and install libsndfile with its dependencies:
+pushd /tmp
+
+curl -O -L https://downloads.xiph.org/releases/ogg/libogg-1.3.5.tar.xz
+curl -O -L https://downloads.xiph.org/releases/vorbis/libvorbis-1.3.7.tar.xz
+curl -O -L https://downloads.xiph.org/releases/flac/flac-1.4.3.tar.xz
+curl -O -L https://downloads.xiph.org/releases/opus/opus-1.4.tar.gz
+curl -O -L https://github.com/libsndfile/libsndfile/releases/download/1.2.2/libsndfile-1.2.2.tar.xz
+sha256sum -c "${package_dir}/utils/SHA256SUMS.txt"
+
+tar xf libogg-1.3.5.tar.xz
+pushd libogg-1.3.5
+./configure
+make -j "$(nproc --all)"
+make install
+ldd --version |& grep -q "musl libc" || ldconfig
+popd
+
+tar xf libvorbis-1.3.7.tar.xz
+pushd libvorbis-1.3.7
+./configure
+make -j "$(nproc --all)"
+make install
+ldd --version |& grep -q "musl libc" || ldconfig
+popd
+
+tar xf flac-1.4.3.tar.xz
+pushd flac-1.4.3
+./configure --disable-cpplibs --disable-programs
+make -j "$(nproc --all)"
+make install
+ldd --version |& grep -q "musl libc" || ldconfig
+popd
+
+tar xf opus-1.4.tar.gz
+pushd opus-1.4
+./configure --disable-static --disable-doc --disable-extra-programs
+make -j "$(nproc --all)"
+make install
+ldd --version |& grep -q "musl libc" || ldconfig
+popd
+
+tar xf libsndfile-1.2.2.tar.xz
+pushd libsndfile-1.2.2
+./configure --disable-full-suite --disable-mpeg
+make -j "$(nproc --all)"
+make install
+ldd --version |& grep -q "musl libc" || ldconfig
+popd
+
+popd
 
 # Py_LIMITED_API 0x03070000 is defined, so it's sufficient to build
 # just one wheel with any Python version:
-python3.8 -m build "${package_dir}" --outdir "${dist_dir}"
+python3.8 -m build "${package_dir}"
 
 # vendor required shared libraries in wheels:
 for wheel in "${dist_dir}"/*.whl
@@ -32,6 +87,6 @@ do
         continue
     fi
 
-    auditwheel repair "${wheel}" --plat "${platform}" --strip --wheel-dir "${dist_dir}"
+    auditwheel repair "${wheel}" --plat "${PLAT}" --strip --wheel-dir "${wheel_dir}"
     rm -v "${wheel}"
 done
