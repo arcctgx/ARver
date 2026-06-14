f66afcf49b568259acda8d5d51902be49ebc0f0b
arcctgx <arcctgx@o2.pl>
2026-06-15 00:14:50 +0200

    remove audio.version module
    This module was introduced in e6371c6, but in retrospect it just wasn't
    a good idea. The function defined in this module is so simple it doesn't
    need a Python wrapper, especially that type stubs are available now. So
    libsndfile_version() can just as well be imported directly from the C
    extension.

--- arver/audio/version.py
+++
@@ -1,8 +0,0 @@
-"""Functions for getting version strings."""
-
-from arver.audio import _audio
-
-
-def get_libsndfile_version() -> str:
-    """Return libsndfile version string."""
-    return _audio.libsndfile_version()
--- arver/version.py
+++ arver/version.py
@@ -1,10 +1,10 @@
 """ARver version string module."""
 
 from arver import APPNAME, VERSION
-from arver.audio.version import get_libsndfile_version
+from arver.audio._audio import libsndfile_version
 
 
 def version_string() -> str:
     """Return full ARver version string, including libsndfile version."""
     number = VERSION.lstrip('v')
-    return f'{APPNAME}-{number} ({get_libsndfile_version()})'
+    return f'{APPNAME}-{number} ({libsndfile_version()})'
